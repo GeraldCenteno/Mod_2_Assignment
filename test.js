2a14a142df2ec69b91414007ee149db941331b3b
GeraldCenteno <geraldcenteno@student.olympic.edu>
2024-04-21 11:05:41 -0700

    moved all javascript to test.js file

---
+++ test.js
@@ -0,0 +1,15 @@
+//JavaScript goes here
+console.log("Hello, World!")
+
+/* 3 Examples of basic arithmetic in JS */
+console.log(3 + 9)
+console.log(3 * 9)
+console.log(9 % 3)
+
+/* 2 Examples of declaring variables in JS */
+let a = 10
+var b = 25
+
+/* 2 Examples of using variables in arithmetic operations in JS */
+console.log(b / a)
+console.log(a - b)
