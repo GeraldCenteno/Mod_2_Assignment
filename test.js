c0ae348bb69ed0833dc0bb613a4b69f4cad7ee27
GeraldCenteno <geraldcenteno@student.olympic.edu>
2024-04-28 17:31:59 -0700

    experimented more with test.js

--- test.js
+++ test.js
@@ -13,3 +13,23 @@ var b = 25
 /* 2 Examples of using variables in arithmetic operations in JS */
 console.log(b / a)
 console.log(a - b)
+
+/* experimenting with DOM manipulation */
+
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+container.appendChild(content);
+
+const btn1 = document.querySelector("#btn1");
+btn1.onclick = () => alert("Hello World");
+
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click", () => {
+  alert("Hello World");
+});
+
+
+  
